a9e4c3cee7d7b9ea9ab4913463a3555518b4e531
XYIAN <kylexyiandilbeck@gmail.com>
2025-10-04 14:16:49 -0700

    init

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    apps: [{
+        name: 'arch2-discord-bot',
+        script: 'index.js',
+        instances: 1,
+        exec_mode: 'fork',
+        env: {
+            NODE_ENV: 'development',
+            PORT: 3000
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            PORT: 3000
+        },
+        error_file: './logs/err.log',
+        out_file: './logs/out.log',
+        log_file: './logs/combined.log',
+        time: true,
+        watch: false,
+        max_memory_restart: '1G',
+        restart_delay: 4000,
+        max_restarts: 10,
+        min_uptime: '10s'
+    }]
+};
